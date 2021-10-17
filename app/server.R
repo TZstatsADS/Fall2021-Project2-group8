a95038447fe6388414a02d4d2604b9fac9bf8898
Apple <apple@AppledeMBP.fios-router.home>
2021-10-17 00:34:58 -0400

    finish the interactive map and all ui design

--- app/server.R
+++ app/server.R
@@ -4,16 +4,17 @@ library(lubridate)
 
 # Read the raw data
 # covid_raw <- read.csv("./Fall2021-Project2-group8/data/cases-by-day.csv")
-shoot_his_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Shooting_Incident_Data__Historic_.csv")
-shoot_cur_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Shooting_Incident_Data__Year_To_Date_.csv")
 
-names(shoot_cur_raw)[length(names(shoot_cur_raw))]<-"Lon_Lat"
-shoot_his_raw$X_COORD_CD = as.character(shoot_his_raw$X_COORD_CD)
-shoot_his_raw$Y_COORD_CD = as.character(shoot_his_raw$Y_COORD_CD)
-
-shoot = dplyr::bind_rows(shoot_his_raw, shoot_cur_raw)
-shoot = shoot[!(is.na(shoot$X_COORD_CD) &!is.na(shoot$Y_COORD_CD)),]
-shoot = shoot %>% arrange(mdy(shoot$OCCUR_DATE))
+# shoot_his_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Shooting_Incident_Data__Historic_.csv")
+# shoot_cur_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Shooting_Incident_Data__Year_To_Date_.csv")
+# 
+# names(shoot_cur_raw)[length(names(shoot_cur_raw))]<-"Lon_Lat"
+# shoot_his_raw$X_COORD_CD = as.character(shoot_his_raw$X_COORD_CD)
+# shoot_his_raw$Y_COORD_CD = as.character(shoot_his_raw$Y_COORD_CD)
+# 
+# shoot = dplyr::bind_rows(shoot_his_raw, shoot_cur_raw)
+# shoot = shoot[!(is.na(shoot$X_COORD_CD) &!is.na(shoot$Y_COORD_CD)),]
+# shoot = shoot %>% arrange(mdy(shoot$OCCUR_DATE))
 
 complaint_his_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Complaint_Map__Historic_.csv")
 complaint_cur_raw <- read.csv("/Users/apple/Fall2021-Project2-group8/data/NYPD_Complaint_Map__Year_to_Date_.csv")
@@ -23,8 +24,28 @@ complaint = complaint[!(is.na(complaint$X_COORD_CD) &!is.na(complaint$Y_COORD_CD
 complaint = complaint %>% arrange(mdy(complaint$CMPLNT_FR_DT))
 
 
-selectShoot <- function(month){
-    shoot[substr(shoot$OCCUR_DATE, 4, 10) == month, ]
+
+# cat = unique(complaint$OFNS_DESC)
+# num = vector()
+# 
+# for (i in cat){
+#     num <- c(num, sum(complaint$OFNS_DESC == i))
+# }
+# 
+# type_crime = vector()
+# 
+# 
+# # choose top 8 
+# for (i in 1:8){
+#     type_crime <- c(type_crime, cat[which.max(num)])
+#     num[which.max(num)] = 0
+# }
+
+
+
+
+selectComplaint <- function(month, type){
+    complaint[(substr(complaint$CMPLNT_FR_DT, 4, 10) == month & complaint$OFNS_DESC == type), ]
 }
 
 
@@ -42,69 +63,142 @@ server <- function(input, output) {
                     L.control.zoom({ position: 'bottomright' }).addTo(this)
                 }"
             ) %>%
-            addProviderTiles("CartoDB.Positron") %>%
+            addProviderTiles("CartoDB.Voyager") %>%
             setView(lng = -73.935242, lat = 40.730610, zoom = 10)
     })
     #
     #
-    df_react_shoot <- reactive({
-        selectshoot(shoot, input$Date)
+    df_react_cm <- reactive({
+        selectComplaint(input$Date,"CRIMINAL MISCHIEF & RELATED OF")
     })
     
+    df_react_gl <- reactive({
+        selectComplaint(input$Date,"GRAND LARCENY")
+    })
     
+    df_react_bu <- reactive({
+        selectComplaint(input$Date,"BURGLARY")
+    })
     
+    df_react_fa <- reactive({
+        selectComplaint(input$Date,"FELONY ASSAULT")
+    })
+
+    df_react_mpl <- reactive({
+        selectComplaint(input$Date, "MISCELLANEOUS PENAL LAW")
+    })
+    
+    df_react_glom <- reactive({
+        selectComplaint(input$Date, "GRAND LARCENY OF MOTOR VEHICLE")
+    })
+    
+    df_react_ro <- reactive({
+        selectComplaint(input$Date, "ROBBERY")
+    })
+    
+    df_react_dw <- reactive({
+        selectComplaint(input$Date, "DANGEROUS WEAPONS")
+    })    
+       
     observe({
-        
         leafletProxy("map", data = df) %>%
             clearShapes() %>%
             clearMarkers() %>%
-            addProviderTiles("CartoDB.Positron") %>%
+            addProviderTiles("CartoDB.Voyager") %>%
             fitBounds(-74.354598, 40.919500, -73.761545, 40.520024)
-        
-        # if (is.null(input$Date)) {
-        #     leafletProxy("map", data = selectshoot(shoot, '03/2020')) %>%
-        #         addCircleMarkers(
-        #             lng=~longitude,
-        #             lat=~latitude,
-        #             # radius = ~ifelse(type == "ship", 6, 10),
-        #             # color = ~pal(type),
-        #             stroke = FALSE,
-        #             fillOpacity = 0.5
-        #         )
-        #     # addAwesomeMarkers(~longitude, ~latitude,
-        #     #                   icon=, label=~name, popup=~content)
-        #     
-        # }
-        
-        
-        
-        # if (input$Date == '03/2020') {
-        #     leafletProxy("map", data = df_react_shoot()) %>%
-        #         addCircleMarkers(
-        #             lng=~longitude,
-        #             lat=~latitude,
-        #             # radius = ~ifelse(type == "ship", 6, 10),
-        #             # color = ~pal(type),
-        #             stroke = FALSE,
-        #             fillOpacity = 0.5
-        #         )
-        #     # addAwesomeMarkers(~longitude, ~latitude,
-        #     #                   icon=, label=~name, popup=~content)
-        #     
-        # }
-    }) 
     
-    # FilterPoints <- function(ds) {
-    #     result <- shoot %>% dplyr::filter(borough == input$plot_borough)
-    #     return(result)
-    # }
-    # FreeMeals_reactive <- reactive({FilterPoints(FreeMeals)})
-    # AdultExerciseEquip_reactive <- reactive({FilterPoints(AdultExerciseEquip)})
-    # Playgrounds_reactive <- reactive({FilterPoints(Playgrounds)})
-    # #Playgrounds_reactive <- reactive({
-    # #   Playgrounds %>% dplyr::filter(borough == input$plot_borough)
-    # #})
-    #
+    
+        if (input$Date != '') {
+            if (input$criminal_mischief){
+                leafletProxy("map", data = df_react_cm()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'blue',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            } 
+            
+            if (input$grand_larceny){
+                leafletProxy("map", data = df_react_gl()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'red',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            } 
+            
+            if (input$burglary){
+                leafletProxy("map", data = df_react_bu()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'orange',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+            
+            if (input$felony_assault){
+                leafletProxy("map", data = df_react_fa()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'gold',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+            
+            if (input$miscellaneous_penal_law){
+                leafletProxy("map", data = df_react_mpl()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'green',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+            
+            if (input$motor_vehicle){
+                leafletProxy("map", data = df_react_glom()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'yellow',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+            
+            if (input$robbery){
+                leafletProxy("map", data = df_react_ro()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'purple',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+            
+            if (input$dangerous_weapons){
+                leafletProxy("map", data = df_react_dw()) %>%
+                    addCircleMarkers(
+                        lng=~Longitude,
+                        lat=~Latitude,
+                        color = 'brown',
+                        stroke = FALSE,
+                        fillOpacity = 0.5
+                    )
+            }
+ 
+        }
+    }) 
     
     groupInput <- reactive({
         switch(input$group1,
--- app/ui.R
+++ app/ui.R
@@ -12,7 +12,6 @@ library(shinythemes)
 library(leaflet)
 
 
-
 # 02/29/2020 - 06/30/2021
 
 shinyUI(
@@ -28,7 +27,7 @@ shinyUI(
                     align = "left"),
                  selectInput("Date",
                              label = "Date", 
-                             choices = c('03/2020', '04/2020','05/2020','06/2020','07/2020','08/2020','09/2020','10/2020','11/2020','12/2020', '01/2021','02/2021','03/2021','04/2021', '05/2021','06/2021')
+                             choices = c('03/2020', '04/2020','05/2020','06/2020','07/2020','08/2020','09/2020','10/2020','11/2020','12/2020', '01/2021','02/2021','03/2021','04/2021', '05/2021','06/2021') 
                              ),
                  h2("Please select", 
                     align = "left"),
@@ -38,7 +37,7 @@ shinyUI(
                              label = "grand larceny", value = FALSE),
                  checkboxInput("burglary", 
                              label = "burglary", value = FALSE),
-                 checkboxInput("felony assault", 
+                 checkboxInput("felony_assault", 
                              label = "felony assault", value = FALSE),
                  checkboxInput("miscellaneous_penal_law", 
                              label = "miscellaneous penal law", value = FALSE),
@@ -70,32 +69,56 @@ shinyUI(
                sidebarPanel(
                  selectInput("NYC borough",
                              label = "borough",
-                             choices = c('a', 'b', 'c')
+                             choices = c('Manhattan', 'Bronx', 'Queens', 'Brooklyn', 'Staten Island')
                  ),
                  selectInput("Type of crime",
                              label = "Type of crime",
-                             choices = c('a', 'b', 'c')
-                 ),
-               checkboxInput("lockdown", label = "lockdown", value = FALSE),  
-               checkboxInput("reopen", label = "reopen", value = FALSE)
-               )),
+                             choices = c('criminal mischief', 'grand larceny', 'burglary', "felony assault", "miscellaneous penal law", "grand larceny of motor vehicle", "robbery", "dangerous_weapons")
+                 )
+               # checkboxInput("lockdown", label = "lockdown", value = FALSE),  
+               # checkboxInput("reopen", label = "reopen", value = FALSE)
+               ),
+               mainPanel()
+               ),
+      # [1] "ANTI-MALE HOMOSEXUAL (GAY)"        "ANTI-WHITE"                       
+      # [3] "ANTI-MUSLIM"                       "ANTI-HISPANIC"                    
+      # [5] "ANTI-TRANSGENDER"                  "ANTI-JEWISH"                      
+      # [7] "ANTI-ASIAN"                        "ANTI-BLACK"                       
+      # [9] "ANTI-FEMALE HOMOSEXUAL (LESBIAN)"  "ANTI-ARAB"                        
+      # [11] "ANTI-CATHOLIC"                     "ANTI-GENDER NON-CONFORMING"       
+      # [13] "ANTI-FEMALE"                       "ANTI-LGBT (MIXED GROUP)"          
+      # [15] "ANTI-MULTI-RACIAL GROUPS"          "ANTI-OTHER ETHNICITY"             
+      # [17] "60 YRS AND OLDER"                  "ANTI-HINDU"                       
+      # [19] "ANTI-BUDDHIST"                     "ANTI-JEHOVAHS WITNESS"            
+      # [21] "ANTI-PHYSICAL DISABILITY"          "ANTI-OTHER RELIGION"              
+      # [23] "ANTI-RELIGIOUS PRACTICE GENERALLY"
         tabPanel("Plots",
                  sidebarPanel(
-                   selectInput("Bias motivated",
-                               label = "bias",
-                               choices = c('a', 'b', 'c')
-                   ),
+                   
                    selectInput("NYC borough",
                                label = "borough",
-                               choices = c('a', 'b', 'c')
+                               choices = c('Manhattan', 'Bronx', 'Queens', 'Brooklyn', 'Staten Island')
                    ),
-                   selectInput("Seriousness",
-                               label = "seriouness",
-                               choices = c('a', 'b', 'c')
+                   selectInput("Bias motivated",
+                               label = "bias",
+                               choices = c('Anti-male homosexual (gay)', 
+                                           "Anti-White", 'Anti-Muslim', 'Anti-Hispanic', 
+                                           'Anti-transgender', 'Anti-Jewish', 'Anti-Asian', 
+                                           'Anti-Black', 'Anti-female homosexual (lesbian)',
+                                           'Anti-Arab', 'Anti-Catholic', 'Anti-gender non-conforming',
+                                           'Anti-female', 'Anti-LGBT (mixed group)', 'Anti-multi-racial group',
+                                           'Anti-other ethnicity', '60 yrs and older', 'Anti-Hindu', 
+                                           'Anti-Buddhist', 'Anti-Jehovahs Witness', 'Anti-physical disability',
+                                           'Anti-other religion', 'Anti-religious practice generally')
+                   )
+                   # selectInput("Seriousness",
+                   #             label = "seriouness",
+                   #             choices = c('a', 'b', 'c')
+                   # )
+                   # checkboxInput("lockdown", label = "lockdown", value = FALSE),  
+                   # checkboxInput("reopen", label = "reopen", value = FALSE)
                    ),
-                   checkboxInput("lockdown", label = "lockdown", value = TRUE),  
-                   checkboxInput("reopen", label = "reopen", value = FALSE)
-                 )
+                  mainPanel()
                  ), 
         tabPanel("Appendix"))
 )
